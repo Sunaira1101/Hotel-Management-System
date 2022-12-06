bd16c893a51ab0101160f9a2f0dcad3362125f09
Sunaira1101 <fahrin.sunaira@northsouth.edu>
2022-12-07 01:38:14 +0600

    new entry

---
+++ HOTELMANAGEMENT/admin/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin - Login</title>
+
+    <?php require('links.php'); ?>
+
+</head>
+<body style="color:rgb(37, 22, 4) ; background-color:rgb(243, 228, 210);">
+
+    <div>
+        <form>
+            <h2>ADMIN LOGIN</h2>
+            <div>
+                
+            </div>
+
+
+        </form>
+
+
+
+    </div>
+    
+
+
+
+<?php require('links.php'); ?>
+
+</body>
+</html>
