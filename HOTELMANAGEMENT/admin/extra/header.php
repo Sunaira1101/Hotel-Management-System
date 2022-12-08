f5a35a3a1ed287a450ff7f64a5dcd2dbce8dec77
Sunaira1101 <fahrin.sunaira@northsouth.edu>
2022-12-08 16:08:09 +0600

    changes

---
+++ HOTELMANAGEMENT/admin/extra/header.php
@@ -0,0 +1,45 @@
+<!-- top bar -->
+
+<div class="container-fluid bg-dark text-light p-3 d-flex align-items-center justify-content-between sticky-top">
+    <h2 class="mb-0 font-three">HOTEL PARADISE</h2>
+    <a href="logout.php" class="btn btn-light btn-sm">LOG OUT</a>
+</div>
+
+
+
+<!-- side panel -->
+
+  <div class="panel col-2 bg-dark border-top border-3 border-secondary">
+    
+    <nav class="navbar navbar-expand-lg navbar-dark ">
+      <div class="container-fluid flex-column align-items-stretch">   <!--to make one after another not sideways-->
+      <h2 class="mt-2 text-center text-white fs-4">ADMIN PANEL</h2>
+      <h3 class="underline"></h3>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#adminDropdown" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse flex-column mt-3 align-items-stretch" id="adminDropdown">
+        
+        <ul class="nav nav-pills flex-column mt-3">
+          <li class="nav-item">
+            <a class="nav-link text-white" href="dashboard.php">Dashboard</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-white" href="#">Users</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-white" href="#">Rooms</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-white" href="settings.php">Settings</a>
+          </li>
+         
+        </ul>
+
+        
+  
+       
+      </div>
+    </div>
+  </nav>
+  </div>
