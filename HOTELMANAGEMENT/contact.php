c2157f8a1a0f69bda8ced3adb2b712becde8737f
Sunaira1101 <fahrin.sunaira@northsouth.edu>
2022-12-10 11:40:22 +0600

    frontend done

--- HOTELMANAGEMENT/contact.php
+++ HOTELMANAGEMENT/contact.php
@@ -110,6 +110,18 @@
   </div>
   <br><br><br>
 
+  <!-- reach us form frontend -->
+  <?php
+
+    if(isset($_POST['send'])){
+      $frm_data = filteration($_POST);
+
+      $q = "INSERT INTO `user_reach`(`name`, `email`, `phone`, `message`) VALUES (?,?,?,?)";
+      $values = [$frm_data['name'],$frm_data['email'],$frm_data['phone'],$frm_data['message']];
+      $res = insert($q,$values,'ssss');
+    }
+  ?>
+
 
 
 
