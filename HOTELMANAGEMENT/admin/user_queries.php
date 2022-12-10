69ff7c4d5c5b45d6028cc69b8b4a4db6e8dedc78
Sunaira1101 <fahrin.sunaira@northsouth.edu>
2022-12-10 12:37:31 +0600

    edits

--- HOTELMANAGEMENT/admin/user_queries.php
+++ HOTELMANAGEMENT/admin/user_queries.php
@@ -31,7 +31,7 @@
         <div class="card shadow border-0 mb-4">
           <div class="card-body">
 
-            <div class="table" style="height: 400px;overflow-y:scroll;">
+            <div class="table" style="height: 400px;overflow-y:scroll;overflow-x:scroll;">
                 <table class="table table-hover border border-4 border-light">
                   <thead class="sticky-top">
                     <tr class="bg-dark text-white">
@@ -39,12 +39,36 @@
                       <th scope="col">Name</th>
                       <th scope="col">Email</th>
                       <th scope="col">Phone No.</th>
-                      <th scope="col">Message</th>
+                      <th scope="col" width="30%">Message</th>
                       <th scope="col">Date</th>
                       <th scope="col">Action</th>
                     </tr>
                   </thead>
                   <tbody>
+                    <?php
+                      
+                      $q = "SELECT * FROM `user_reach` ORDER BY `reach_ID` DESC"; //so that newer msgs at top
+                      $data = mysqli_query($db, $q);
+                      $no=1;
+
+                      while($row = mysqli_fetch_assoc($data)){
+                        echo<<<query
+                         <tr>
+                           <td>$no</td>
+                           <td>$row[name]</td>
+                           <td>$row[email]</td>
+                           <td>$row[phone]</td>
+                           <td>$row[message]</td>
+                           <td>$row[date]</td>
+                         </tr>
+
+                        query;
+                        $no++;
+                      }
+
+
+
+                    ?>
                     
                     
                   </tbody>
