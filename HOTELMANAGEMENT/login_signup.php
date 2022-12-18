e62d757bebfaf84ecee977c31f307387e917e54c
Sunaira1101 <fahrin.sunaira@northsouth.edu>
2022-12-19 00:55:44 +0600

    mail error

--- HOTELMANAGEMENT/login_signup.php
+++ HOTELMANAGEMENT/login_signup.php
@@ -2,6 +2,30 @@
   
   require('admin/extra/func.php');
   require('admin/extra/connect.php');
+  require("sendgrid/sendgrid-php.php");
+
+  function send_mail(){
+      
+      $email = new \SendGrid\Mail\Mail(); 
+      $email->setFrom("test@example.com", "Example User");
+      $email->setSubject("Sending with SendGrid is Fun");
+      $email->addTo("test@example.com", "Example User");
+      $email->addContent("text/plain", "and easy to do anywhere, even with PHP");
+      $email->addContent(
+          "text/html", "<strong>and easy to do anywhere, even with PHP</strong>"
+      );
+      $sendgrid = new \SendGrid(getenv('SENDGRID_API_KEY'));
+      try {
+          $response = $sendgrid->send($email);
+          print $response->statusCode() . "\n";
+          print_r($response->headers());
+          print $response->body() . "\n";
+      } catch (Exception $e) {
+          echo 'Caught exception: '. $e->getMessage() ."\n";
+      }
+    }
+
+
 
   if(isset($_POST['signup'])){
 
@@ -24,7 +48,7 @@
 
     //send confirmation link
 
-    
+
 
 
 
