e206df518f7d66bf570d70ef2128ff15010eb5cb
Sunaira1101 <fahrin.sunaira@northsouth.edu>
2022-12-20 13:41:28 +0600

    edits

--- HOTELMANAGEMENT/pay_status.php
+++ HOTELMANAGEMENT/pay_status.php
@@ -3,6 +3,18 @@
 require('admin/extra/func.php');
 require('admin/extra/connect.php');
 
+session_start();
+
+if(!(isset($_SESSION['login']) && $_SESSION['login']==true)){
+    redirect('room.php');
+}
+
+$frm_data = filteration($_POST);
+
+$q = "INSERT INTO `booking_details`(`user_id`, `room_id`, `room_name`, `price`, `total_pay`, `user_name`, `phonenum`, `address`) VALUES (?,?,?,?,?,?,?,?)";
+
+// insert($q,[$_SESSION['room']['name']]);
+
 
 
 
