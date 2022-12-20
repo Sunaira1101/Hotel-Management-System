4c1f1115fe2aaa2f4c4fbaf53f67d94ae986624a
Farasha08 <farasha.yussouf@northsouth.edu>
2022-12-21 02:31:17 +0600

    modified:   HOTELMANAGEMENT/admin/dashboard.php 	modified:   HOTELMANAGEMENT/admin/extra/essentials.php

---
+++ HOTELMANAGEMENT/admin/dashboard.php
@@ -0,0 +1,20 @@
+<?php
+    require('extra/essentials.php');
+    adminLogin()
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Panel -  Dashboard </title>
+    <?php require('extra/links.php'); ?>
+</head>
+<body>
+  
+
+<?php require('extra/scripts.php'); ?>
+</body>
+</html>
--- HOTELMANAGEMENT/admin/extra/essentials.php
+++ HOTELMANAGEMENT/admin/extra/essentials.php
@@ -17,6 +17,15 @@ function redirect($url){
 
 }
 
+function adminLogin(){
+    session_start();
+    if(!(isset($_SESSION['adminLogin']) && $_SESSION['adminLogin'] == true)){
+        echo "<script>   
+        window.location.href= 'index.php';
+        </script>";
+    }
+}
+
 
 
 ?>
